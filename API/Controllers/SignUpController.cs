96e76b8bad5d8fc08787b6d18004b03cba091be1
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-04-28 20:52:17 -0300

    Substitui WeatherForecast por SignUpController
    Removidos os arquivos `WeatherForecastController.cs` e
    `WeatherForecast.cs`, que forneciam previsões do tempo.
    
    Adicionado o arquivo `SignUpController.cs`, contendo um
    novo controlador API com um endpoint HTTP POST para a
    funcionalidade de "Sign Up". Atualmente, o método `SignUp`
    retorna apenas um status HTTP 200 (OK) sem lógica adicional.

---
+++ API/Controllers/SignUpController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignUpController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SignUp()
+        {
+            return Ok();
+        }
+    }
+}
