db0bcefeaeb194cf0eb61e8f344f8e11f2ba809b
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-04-29 10:54:32 -0300

    Adiciona validação de documento e teste de integração
    Implementada validação para o campo `Document` no `SignUpDtoValidator`,
    incluindo regras para verificar a validade do documento com métodos
    auxiliares como `ValidateDocument`, `CleanDocument` e outros.
    
    Adicionado teste de integração no `SignUpControllerTests` para garantir
    que documentos inválidos retornem status HTTP 422 e mensagem "Invalid
    document". Cenários testados incluem documentos nulos, curtos, com
    caracteres não numéricos e outros formatos inválidos.

--- API/Validators/SignUpDtoValidator.cs
+++ API/Validators/SignUpDtoValidator.cs
@@ -15,6 +15,69 @@ namespace API.Validators
             RuleFor(x => x.Email)
                 .Must(email => !string.IsNullOrEmpty(email) && Regex.IsMatch(email, @"^(.+)\@(.+)$"))
                 .WithMessage("Invalid email");
+
+            RuleFor(x => x.Document)
+                .Must(ValidateDocument)
+                .WithMessage("Invalid document");
+        }
+
+        private bool ValidateDocument(string? document)
+        {
+            const int VALID_LENGTH = 11;
+
+            if (document is null)
+            {
+                return false;
+            }
+
+            document = CleanDocument(document);
+
+            if (document.Length != VALID_LENGTH)
+            {
+                return false;
+            }
+
+            if (AllDigitsEqual(document))
+            {
+                return false;
+            }
+
+            var digit1 = CalculateDigit(document, 10);
+            var digit2 = CalculateDigit(document, 11);
+            return ExtractDigit(document).Equals($"{digit1}{digit2}");
+        }
+
+        private string CleanDocument(string document)
+        {
+            return document.Trim()
+                .Replace(".", "")
+                .Replace("-", "");  
+        }
+
+        private bool AllDigitsEqual(string document)
+        {
+            var firstDigit = document[0];
+            return document.All(digit => digit.Equals(firstDigit));
+        }
+
+        private int CalculateDigit(string document, int factor)
+        {
+            var total = 0;
+            foreach (var digit in document)
+            {
+                if (factor > 1)
+                {
+                    total += (digit - '0') * factor--;
+                }
+            }
+
+            var rest = total % 11;
+            return (rest < 2) ? 0 : 11 - rest;
+        }
+
+        private string ExtractDigit(string document)
+        {
+            return document.Substring(document.Length - 2, 2);
         }
     }
 }
