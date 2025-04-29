3a1b07f744ac9a4d325416abb93e451bb1504b4d
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-04-28 21:59:29 -0300

    Definir rota explícita para o SignUpController
    A rota do controlador foi alterada de `[Route("api/[controller]")]`
    para `[Route("api/signup")]`. Isso padroniza e simplifica a rota,
    evitando dependência do nome do controlador para a geração dinâmica.

--- API/Controllers/SignUpController.cs
+++ API/Controllers/SignUpController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/signup")]
     [ApiController]
     public class SignUpController : ControllerBase
     {
