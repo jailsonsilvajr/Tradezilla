7b597627d31955eadce54ca087d8bf9d6fc53dc7
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-04-28 21:07:06 -0300

    Adicionado projeto de testes e melhorias na solução
    - Adicionada a classe parcial `Program` no `Program.cs` para facilitar testes e extensões.
    - Atualizado o arquivo `Tradezilla.sln` para incluir o projeto `Tests` e configurar plataformas de compilação.
    - Criado o `Tests.csproj` com configuração para .NET 8.0, pacotes de teste e referência ao projeto `API`.
    - Adicionada a classe `CustomWebApplicationFactory` para suporte a testes de integração.
    - Criado o arquivo `SignUpControllerTests.cs` com testes para o endpoint `/api/signup`.

---
+++ Tests/Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Tests.Integration
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+    }
+}
---
+++ Tests/Integration/SignUpControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace Tests.Integration
+{
+    public class SignUpControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public SignUpControllerTests(CustomWebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task SignUp_ReturnsOk()
+        {
+            var requestUri = "/api/signup";
+
+            var response = await _client.PostAsync(requestUri, null);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
