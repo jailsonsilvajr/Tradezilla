979e03d3b795b26eacd6ed6303f6313c35c03cd2
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-05-01 13:25:09 -0300

    Adiciona projetos Core e Domain à solução Tradezilla
    - Adicionado o projeto **Core** ao arquivo `Tradezilla.sln`.
    - Adicionado o projeto **Domain** ao arquivo `Tradezilla.sln` com build e debug configurados.
    - Associado o projeto **Domain** ao projeto pai **Core` na seção `NestedProjects`.
    - Criado o arquivo `Domain.csproj` com suporte a .NET 8.0, usings implícitos e valores nulos habilitados.
    - Adicionada a classe `Account` no namespace `Domain.Entities` com propriedades (`AccountId`, `Name`, `Email`, `Document`, `Password`) e construtor para inicialização.

---
+++ Domain/Entities/Account.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Entities
+{
+    public class Account
+    {
+        public Account(Guid accountId, string name, string email, string document, string password)
+        {
+            AccountId = accountId;
+            Name = name;
+            Email = email;
+            Document = document;
+            Password = password;
+        }
+
+        public Guid AccountId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Document { get; set; }
+        public string Password { get; set; }
+    }
+}
