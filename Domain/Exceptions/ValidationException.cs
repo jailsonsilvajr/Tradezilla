4c8abca66db9ab757e007e9cfbcda8186d92ad56
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-05-01 14:54:09 -0300

    Adiciona exceção de validação personalizada
    Foi criada a classe `ValidationException` no namespace
    `Domain.Exceptions`, herdando de `Exception`, para tratar
    erros de validação. A classe inclui uma propriedade
    `Errors` do tipo `List<ValidationFailure>` para armazenar
    os erros e um construtor que aceita uma mensagem e uma
    lista de erros. Também foi adicionado um `using` para
    importar o namespace `FluentValidation.Results`.

---
+++ Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.Results;
+
+namespace Domain.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public List<ValidationFailure> Errors { get; }
+
+        public ValidationException(string message, List<ValidationFailure> errors) 
+            : base(message)
+        {
+            Errors = errors;
+        }
+    }
+}
