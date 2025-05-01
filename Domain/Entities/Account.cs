630059f75b0d036dd6fcd784cef3ac23d7a354d2
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-05-01 14:54:31 -0300

    Refatoração e melhorias na classe Account
    - Alterado o namespace de `Domain.Entities` para incluir `Domain.Exceptions` e `Domain.Validators`.
    - Propriedades `AccountId`, `Name`, `Email`, `Document` e `Password` tornadas somente leitura, garantindo imutabilidade.
    - Construtor público da classe `Account` removido e tornado privado.
    - Adicionado método de fábrica estático `Create` para criação de instâncias com validação e geração de `Guid` para `AccountId`.
    - Integrado validador estático `AccountValidator` para validação de instâncias, lançando `ValidationException` em caso de falha.

--- Domain/Entities/Account.cs
+++ Domain/Entities/Account.cs
@@ -1,8 +1,18 @@
-﻿namespace Domain.Entities
+﻿using Domain.Exceptions;
+using Domain.Validators;
+
+namespace Domain.Entities
 {
     public class Account
     {
-        public Account(Guid accountId, string name, string email, string document, string password)
+        private static readonly AccountValidator _validator = new AccountValidator();
+        public Guid AccountId { get; }
+        public string Name { get; }
+        public string Email { get; }
+        public string Document { get; }
+        public string Password { get; }
+
+        private Account(Guid accountId, string name, string email, string document, string password)
         {
             AccountId = accountId;
             Name = name;
@@ -11,10 +21,16 @@
             Password = password;
         }
 
-        public Guid AccountId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Document { get; set; }
-        public string Password { get; set; }
+        public static Account Create(string name, string email, string document, string password)
+        {
+            var newAccount = new Account(Guid.NewGuid(), name, email, document, password);
+            var validationResult = _validator.Validate(newAccount);
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException("", validationResult.Errors);
+            }
+
+            return newAccount;
+        }
     }
 }
