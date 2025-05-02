003fe6a7bf63d715f7819edab763776883aa1792
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-05-01 21:26:49 -0300

    Adiciona caso de uso para cadastro de contas
    Foi criada a interface `ISignUp` no namespace `Application.Ports.Driving`,
    definindo o método assíncrono `SingUpAsync` (possível erro de digitação,
    provavelmente deveria ser `SignUpAsync`), que recebe um `AccountDto` e
    retorna um `Guid`.
    
    Também foi implementada a classe `SignUpUseCase` no namespace
    `Application.UseCases`, que utiliza `_accountRepository` e `_unitOfWork`
    para registrar contas e salvar alterações no banco de dados. O método
    `SingUpAsync` cria uma nova conta, registra-a no repositório e retorna
    o `AccountId` da conta criada.

---
+++ Application/Ports/Driving/ISignUp.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs;
+
+namespace Application.Ports.Driving
+{
+    public interface ISignUp
+    {
+        Task<Guid> SingUpAsync(AccountDto accountDto);
+    }
+}
---
+++ Application/UseCases/SignUpUseCase.cs
@@ -0,0 +1,33 @@
+﻿using Application.DTOs;
+using Application.Ports.Driven;
+using Application.Ports.Driving;
+using Domain.Entities;
+
+namespace Application.UseCases
+{
+    public class SignUpUseCase : ISignUp
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SignUpUseCase(IAccountRepository accountRepository, IUnitOfWork unitOfWork)
+        {
+            _accountRepository = accountRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Guid> SingUpAsync(AccountDto accountDto)
+        {
+            var account = Account.Create(
+                accountDto.Name,
+                accountDto.Email,
+                accountDto.Document,
+                accountDto.Password);
+
+            _accountRepository.RegisterAccount(account);
+            await _unitOfWork.SaveChangesAsync();
+
+            return account.AccountId;
+        }
+    }
+}
