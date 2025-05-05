0255acb76b3bc4ec4b934280e0bd40b4e3f136dc
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-05-05 15:57:28 -0300

    Adiciona novas propriedades à classe AccountDto
    Foram adicionadas três novas propriedades à classe `AccountDto` no arquivo `AccountDto.cs`:
    - `Name` (string?): Nome associado à conta.
    - `Email` (string?): Endereço de e-mail associado à conta.
    - `Document` (string?): Documento associado à conta.

--- Application/DTOs/AccountDto.cs
+++ Application/DTOs/AccountDto.cs
@@ -2,6 +2,9 @@
 {
     public class AccountDto
     {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Document { get; set; }
         public List<AssetDto>? Assets { get; set; }
     }
 }
