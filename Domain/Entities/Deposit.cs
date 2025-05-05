c93e779cdcfc6cc3ba56b36a68cd80f4d4deb79c
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-05-05 15:14:26 -0300

    Tornar a classe Deposit imutável e ajustar AssetId
    A classe `Deposit` foi refatorada para reforçar sua imutabilidade:
    - Propriedades agora são somente leitura (`get`), sem `set`.
    - O tipo da propriedade `AssetId` foi alterado de `string` para `string?`.
    - O construtor privado e o método estático `Create` foram ajustados para aceitar `assetId` como `string?`.
    - Propriedades públicas com `set` foram removidas, permitindo atribuição apenas no momento da construção do objeto.

--- Domain/Entities/Deposit.cs
+++ Domain/Entities/Deposit.cs
@@ -7,20 +7,20 @@ namespace Domain.Entities
     {
         public static readonly int MAX_ASSETID_LENGTH = 5;
         private static readonly DepositValidator _validator = new DepositValidator();
-        public Guid DepositId { get; set; }
-        public Guid AccountId { get; set; }
-        public string AssetId { get; set; }
-        public decimal Quantity { get; set; }
-        public Account? Account { get; set; }
+        public Guid DepositId { get; }
+        public Guid AccountId { get; }
+        public string? AssetId { get; }
+        public decimal Quantity { get; }
+        public Account? Account { get; }
 
-        private Deposit(Guid accountId, string assetId, decimal quantity)
+        private Deposit(Guid accountId, string? assetId, decimal quantity)
         {
             AccountId = accountId;
             AssetId = assetId;
             Quantity = quantity;
         }
 
-        public static Deposit Create(Guid accountId, string assetId, decimal quantity)
+        public static Deposit Create(Guid accountId, string? assetId, decimal quantity)
         {
             var newDeposit = new Deposit(accountId, assetId, quantity);
             var validationResult = _validator.Validate(newDeposit);
