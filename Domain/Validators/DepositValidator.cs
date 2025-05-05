7f9ef5a892afeb9fa7d65d41c5ff09299756e977
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-05-05 15:10:37 -0300

    Adiciona validador para entidade Deposit
    Implementa a classe DepositValidator como subclasse de AbstractValidator<Deposit> no namespace Domain.Validators. Adiciona validações para as propriedades AccountId, AssetId e Quantity, garantindo consistência nos dados. Inclui mensagens de erro personalizadas e utiliza a biblioteca FluentValidation.

---
+++ Domain/Validators/DepositValidator.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Domain.Validators
+{
+    public class DepositValidator : AbstractValidator<Deposit>
+    {
+        public DepositValidator()
+        {
+            RuleFor(deposit => deposit.AccountId)
+                .NotEmpty()
+                .WithMessage("AccountId cannot be empty");
+
+            RuleFor(deposit => deposit.AssetId)
+                .NotEmpty()
+                .WithMessage("AssetId cannot be empty")
+                .Length(1, Deposit.MAX_ASSETID_LENGTH)
+                .WithMessage($"AssetId must be between 1 and {Deposit.MAX_ASSETID_LENGTH} characters long");
+
+            RuleFor(deposit => deposit.Quantity)
+                .GreaterThan(0)
+                .WithMessage("Quantity must be greater than 0");
+        }
+    }
+}
