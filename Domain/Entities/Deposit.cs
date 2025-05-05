057841ef709e81e3c55ac7710987c5ce1c8abc7d
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-05-05 15:10:55 -0300

    Refatora criação e validação na classe Deposit
    A criação de objetos da classe `Deposit` foi refatorada para
    incluir validação com a classe `DepositValidator`. Agora, o
    construtor da classe é privado, e um método estático `Create`
    foi introduzido para encapsular a lógica de criação e validação.
    Se os dados forem inválidos, uma exceção `ValidationException`
    será lançada.
    
    Além disso, novos namespaces foram importados:
    `Domain.Exceptions` e `Domain.Validators`.

--- Domain/Entities/Deposit.cs
+++ Domain/Entities/Deposit.cs
@@ -1,19 +1,34 @@
-﻿namespace Domain.Entities
+﻿using Domain.Exceptions;
+using Domain.Validators;
+
+namespace Domain.Entities
 {
     public class Deposit
     {
         public static readonly int MAX_ASSETID_LENGTH = 5;
+        private static readonly DepositValidator _validator = new DepositValidator();
         public Guid DepositId { get; set; }
         public Guid AccountId { get; set; }
         public string AssetId { get; set; }
         public decimal Quantity { get; set; }
         public Account? Account { get; set; }
 
-        public Deposit(Guid accountId, string assetId, decimal quantity)
+        private Deposit(Guid accountId, string assetId, decimal quantity)
         {
             AccountId = accountId;
             AssetId = assetId;
             Quantity = quantity;
+        }
+
+        public static Deposit Create(Guid accountId, string assetId, decimal quantity)
+        {
+            var newDeposit = new Deposit(accountId, assetId, quantity);
+            var validationResult = _validator.Validate(newDeposit);
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException("Invalid data to create deposit", validationResult.Errors);
+            }
+            return newDeposit;
         }
     }
 }
