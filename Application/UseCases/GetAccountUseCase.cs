41ab3750c3f15164a34cd5586aa8bfa3e0ff1e3a
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-05-05 15:57:44 -0300

    Adiciona caso de uso para obter conta por accountId
    Introduz a interface `IGetAccount` no namespace `Application.Ports.Driving`, definindo o contrato para a operação `GetAccountByAccountIdAsync`. Implementa a classe `GetAccountUseCase`, que contém a lógica para buscar uma conta pelo `accountId` usando o repositório `IAccountRepository`.
    
    Inclui validação para verificar a existência da conta, lançando uma `EntityNotFoundException` caso não seja encontrada. Adiciona a transformação de dados da entidade de domínio para o DTO `AccountDto`, incluindo a conversão de depósitos em `AssetDto`.
    
    Adiciona os namespaces necessários para suportar as novas implementações.

---
+++ Application/UseCases/GetAccountUseCase.cs
@@ -0,0 +1,38 @@
+﻿using Application.DTOs;
+using Application.Ports.Driven;
+using Application.Ports.Driving;
+using Domain.Exceptions;
+
+namespace Application.UseCases
+{
+    public class GetAccountUseCase : IGetAccount
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public GetAccountUseCase(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public async Task<AccountDto> GetAccountByAccountIdAsync(Guid accountId)
+        {
+            var account = await _accountRepository.GetAccountByAccountIdAsync(accountId);
+            if (account is null)
+            {
+                throw new EntityNotFoundException($"Account {accountId} not found");
+            }
+
+            return new AccountDto
+            {
+                Name = account.Name,
+                Email = account.Email,
+                Document = account.Document,
+                Assets = account.Deposits.Select(d => new AssetDto
+                {
+                    AssetId = d.AssetId,
+                    Quantity = d.Quantity
+                }).ToList()
+            };
+        }
+    }
+}
