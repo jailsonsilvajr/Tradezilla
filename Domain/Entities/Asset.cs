4ec7b6a3dcba97af9ec0ba233ab076bc29a15092
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-05-05 16:30:08 -0300

    Adiciona entidade Asset e validação com FluentValidation
    Introduz a classe `Asset` no domínio para representar ativos,
    com propriedades como `AssetId`, `AssetName`, `Balance` e
    uma coleção de depósitos. Inclui validação robusta via
    `AssetValidator`, que utiliza o FluentValidation para aplicar
    regras de negócio, como limite de tamanho do nome e saldo >= 0.
    
    Corrige o uso da constante `MAX_ASSETNAME_LENGTH` na validação,
    garantindo alinhamento com a classe `Asset`. Essas mudanças
    melhoram a consistência e a confiabilidade do sistema.

---
+++ Domain/Entities/Asset.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Exceptions;
+using Domain.Validators;
+
+namespace Domain.Entities
+{
+    public class Asset
+    {
+        public static readonly int MAX_ASSETNAME_LENGTH = 5;
+        private static readonly AssetValidator _validator = new AssetValidator();
+
+        public Guid AssetId { get; set; }
+        public string? AssetName { get; set; }
+        public decimal Balance { get; set; }
+        public ICollection<Deposit> Deposits { get; set; }
+
+        private Asset(Guid assetId, string? assetName, decimal balance)
+        {
+            AssetId = assetId;
+            AssetName = assetName;
+            Balance = balance;
+            Deposits = new List<Deposit>();
+        }
+
+        public static Asset Create(Guid assetId, string? assetName, decimal balance)
+        {
+            var newAsset =  new Asset(assetId, assetName, balance);
+            var validationResult = _validator.Validate(newAsset);
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException("Invalid data to create asset", validationResult.Errors);
+            }
+
+            return newAsset;
+        }
+    }
+}
---
+++ Domain/Validators/AssetValidator.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Domain.Validators
+{
+    public class AssetValidator : AbstractValidator<Asset>
+    {
+        public AssetValidator()
+        {
+            RuleFor(deposit => deposit.AssetName)
+                .Must(x =>
+                    !string.IsNullOrEmpty(x)
+                    && x.Length > 0 && x.Length <= Deposit.MAX_ASSETID_LENGTH)
+                .WithMessage($"AssetName must be between 1 and {Asset.MAX_ASSETNAME_LENGTH} characters long");
+
+            RuleFor(deposit => deposit.Balance)
+                .GreaterThanOrEqualTo(0)
+                .WithMessage("Balance must be greater than or equal to 0");
+        }
+    }
+}
