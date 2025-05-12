d4537d1fd492fe71d0fc919327660ecbba167853
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-05-12 10:40:06 -0300

    Adiciona entidade Order e validações com FluentValidation
    - Criada a classe `Order` no namespace `Domain.Entities`, representando uma entidade de pedido com propriedades como `OrderId`, `AccountId`, `Market`, `Side`, `Quantity`, `Price`, entre outras.
    - Implementado o método estático `Create` na classe `Order` para instanciar pedidos, validando os dados de entrada com a classe `OrderValidator`. Lança `ValidationException` em caso de dados inválidos.
    - Adicionada a classe `OrderValidator` no namespace `Domain.Validators`, utilizando `FluentValidation` para validar propriedades da entidade `Order` com regras específicas.
    - Criados testes unitários na classe `OrderTests` no namespace `Tests.Unit.Entities` para validar o comportamento da entidade `Order` e suas validações.

---
+++ Domain/Entities/Order.cs
@@ -0,0 +1,61 @@
+﻿using Domain.Exceptions;
+using Domain.Validators;
+
+namespace Domain.Entities
+{
+    public class Order
+    {
+        public static readonly int MAX_MARKET_LENGTH = 7;
+        public static readonly int MAX_SIDE_LENGTH = 5;
+        private static readonly OrderValidator _validator = new OrderValidator();
+        public Guid OrderId { get; }
+        public Guid AccountId { get; set; }
+        public string? Market { get; }
+        public string? Side { get; }
+        public int Quantity { get; }
+        public decimal Price { get; }
+        public int FillQuantity { get; }
+        public decimal FillPrice { get; }
+        public DateTime CreatedAt { get; }
+
+        private Order(
+            Guid orderId, Guid accountId, string? market, 
+            string? side, int quantity, decimal price, 
+            int fillQuantity, decimal fillPrice, DateTime createdAt)
+        {
+            OrderId = orderId;
+            AccountId = accountId;
+            Market = market;
+            Side = side;
+            Quantity = quantity;
+            Price = price;
+            FillQuantity = fillQuantity;
+            FillPrice = fillPrice;
+            CreatedAt = createdAt;
+        }
+
+        public static Order Create(
+            Guid accountId, string? market, string? side,
+            int quantity, decimal price, int fillQuantity, decimal fillPrice)
+        {
+            var newOrder = new Order(
+                Guid.NewGuid(),
+                accountId,
+                market,
+                side,
+                quantity,
+                price,
+                fillQuantity,
+                fillPrice,
+                DateTime.UtcNow);
+
+            var validationResult = _validator.Validate(newOrder);
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException("Invalid data to create order", validationResult.Errors);
+            }
+
+            return newOrder;
+        }
+    }
+}
---
+++ Domain/Validators/OrderValidator.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Domain.Validators
+{
+    public class OrderValidator : AbstractValidator<Order>
+    {
+        public OrderValidator()
+        {
+            RuleFor(order => order.AccountId)
+                .NotEmpty()
+                .WithMessage("AccountId cannot be empty");
+
+            RuleFor(order => order.Market)
+                .NotEmpty()
+                .WithMessage("Market cannot be null, empty or whitespace")
+                .Length(1, Order.MAX_MARKET_LENGTH)
+                .WithMessage($"Market must be between 1 and {Order.MAX_MARKET_LENGTH} characters long");
+
+            RuleFor(order => order.Side)
+                .NotEmpty()
+                .WithMessage("Side cannot be null, empty or whitespace")
+                .Length(1, Order.MAX_SIDE_LENGTH)
+                .WithMessage($"Side must be between 1 and {Order.MAX_SIDE_LENGTH} characters long");
+
+            RuleFor(order => order.Quantity)
+                .GreaterThan(0)
+                .WithMessage("Quantity must be greater than 0");
+
+            RuleFor(order => order.Price)
+                .GreaterThan(0)
+                .WithMessage("Price must be greater than 0");
+
+            RuleFor(order => order.CreatedAt)
+                .NotNull()
+                .WithMessage("CreatedAt cannot be null");
+        }
+    }
+}
