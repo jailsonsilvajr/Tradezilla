20b70cb41e2f63b76a4e0a78a53520e1dc64d5d4
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-05-22 16:04:16 -0300

    Adiciona repositório de pedidos e interface associada
    Adicionada a interface `IOrderRepository` no namespace
    `Application.Ports.Driven`, com o método assíncrono
    `GetOrdersByMarketIdAsync` para buscar pedidos por `marketId`.
    
    Implementada a classe `OrderRepository` no namespace
    `DatabaseContext.Repositories`, que utiliza o contexto
    `TradezillaContext` e o `OrderMapper` para acessar e mapear
    dados do banco de dados para o domínio.

---
+++ DatabaseContext/Repositories/OrderRepository.cs
@@ -0,0 +1,26 @@
+﻿using Application.Ports.Driven;
+using DatabaseContext.Mappers;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseContext.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly TradezillaContext _context;
+
+        public OrderRepository(TradezillaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Order>?> GetOrdersByMarketIdAsync(string marketId)
+        {
+            var ordersModel = await _context.Orders
+                .Where(o => o.Market == marketId)
+                .ToListAsync();
+
+            return ordersModel?.Select(o => OrderMapper.ToDomain(o)).ToList();
+        }
+    }
+}
