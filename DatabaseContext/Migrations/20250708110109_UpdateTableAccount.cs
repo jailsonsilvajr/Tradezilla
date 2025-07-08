e825a2842b08e2eeb10aa02563820a42410049fe
Jailson Silva <jailson.silva@sorteonline.com.br>
2025-07-08 08:01:57 -0300

    Adiciona migração UpdateTableAccount para tabela Accounts
    Esta migração define a estrutura da tabela `Accounts`, incluindo as propriedades `AccountId`, `Document`, `Email`, `Name` e `Password`. A coluna `Password` foi alterada para ser obrigatória, enquanto a coluna `Document` foi atualizada para garantir que não seja nula. Um índice único foi criado para `Document`. Além disso, foram definidos relacionamentos entre as entidades `AccountModel`, `AssetModel`, `OrderModel` e `TransactionModel`, assegurando a integridade referencial. As alterações no arquivo `TradezillaContextModelSnapshot.cs` refletem essas mudanças, removendo a filtragem do índice único na coluna `Document`.

---
+++ DatabaseContext/Migrations/20250708110109_UpdateTableAccount.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_Document",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Accounts",
+                type: "varchar(14)",
+                maxLength: 14,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(14)",
+                oldMaxLength: 14,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                table: "Accounts",
+                type: "varchar(11)",
+                maxLength: 11,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(11)",
+                oldMaxLength: 11,
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_Document",
+                table: "Accounts",
+                column: "Document",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_Document",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Accounts",
+                type: "varchar(14)",
+                maxLength: 14,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(14)",
+                oldMaxLength: 14);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                table: "Accounts",
+                type: "varchar(11)",
+                maxLength: 11,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(11)",
+                oldMaxLength: 11);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_Document",
+                table: "Accounts",
+                column: "Document",
+                unique: true,
+                filter: "[Document] IS NOT NULL");
+        }
+    }
+}
